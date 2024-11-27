41e6c6ca7f7ac92093fb5f3c983037f7d48c6b0e
ivanisovmaks <166316568+ivanisovmaks@users.noreply.github.com>
2024-11-27 14:11:35 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,6 +7,96 @@ document.addEventListener("DOMContentLoaded", function () {
     const loginInput = document.getElementById("login");
     const passwordInput = document.getElementById("password");
     const userNameSpan = document.querySelector(".user-name");
+    const cardsContainer = document.querySelector(".cards.cards-restaurants");
+
+    const restaurants = [
+        {
+            name: "Піца плюс",
+            image: "img/pizza-plus/preview.jpg",
+            time: "50 хвилин",
+            rating: 4.5,
+            price: "від 200 ₴",
+            category: "Піца"
+        },
+        {
+            name: "Танукі",
+			image: "img/tanuki/preview.jpg",
+            time: "60 хвилин",
+            rating: 4.5,
+            price: "От 1 200 ₴",
+            category: "Суші, роли"
+        },
+        {
+            name: "FoodBand",
+			image: "img/food-band/preview.jpg",
+            time: "40 хвилин",
+            rating: 4.5,
+            price: "От 150 ₴",
+            category: "Піца"
+        },
+        {
+            name: "Ikigai",
+			image: "img/palki-skalki/preview.jpg",
+            time: "55 хвилин",
+            rating: 4.5,
+            price: "От 250 ₴",
+            category: "Піца"
+        },
+        {
+            name: "Пузата хата",
+			image: "img/gusi-lebedi/preview.jpg",
+            time: "75 хвилин",
+            rating: 4.5,
+            price: "От 300 ₴",
+            category: "Українські страви"
+        },
+        {
+            name: "PizzaBurger",
+			image: "img/pizza-burger/preview.jpg",
+            time: "45 хвилин",
+            rating: 4.5,
+            price: "От 700 ₴",
+            category: "Піца"
+        }
+    ];
+
+    function generateRestaurantCards() {
+        restaurants.forEach(restaurant => {
+            const cardHTML = `
+                <a href="#" class="card card-restaurant">
+                    <img src="${restaurant.image}" alt="image" class="card-image" />
+                    <div class="card-text">
+                        <div class="card-heading">
+                            <h3 class="card-title">${restaurant.name}</h3>
+                            <span class="card-tag tag">${restaurant.time}</span>
+                        </div>
+                        <div class="card-info">
+                            <div class="rating">${restaurant.rating}</div>
+                            <div class="price">${restaurant.price}</div>
+                            <div class="category">${restaurant.category}</div>
+                        </div>
+                    </div>
+                </a>
+            `;
+            cardsContainer.insertAdjacentHTML("beforeend", cardHTML);
+        });
+    }
+
+    cardsContainer.addEventListener("click", function (event) {
+		const card = event.target.closest(".card-restaurant");
+
+		if (!card) return;
+		
+		event.preventDefault();
+
+		if (!localStorage.getItem("login")) {
+			modalAuth.style.display = "flex";
+			document.body.style.overflow = "hidden";
+		} else {
+			window.location.href = "restaurant.html";
+		}
+	});
+
 
     authButton.addEventListener("click", () => {
         modalAuth.style.display = "flex";
@@ -14,7 +104,9 @@ document.addEventListener("DOMContentLoaded", function () {
         resetInputBorders();
     });
 
-    closeAuthButton.addEventListener("click", closeModal);
+    closeAuthButton.addEventListener("click", () => {
+        closeModal();
+    });
 
     modalAuth.addEventListener("click", (event) => {
         if (event.target === modalAuth) {
@@ -77,4 +169,6 @@ document.addEventListener("DOMContentLoaded", function () {
         loginInput.value = "";
         passwordInput.value = "";
     }
+
+    generateRestaurantCards();
 });
